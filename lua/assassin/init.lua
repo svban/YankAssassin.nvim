11c68db88bcea6af005c0917773c036a009e3ed3
svban <shb1096@gmail.com>
2024-06-26 16:52:32 +0530

    fixed register and count in mappings

--- lua/assassin/init.lua
+++ lua/assassin/init.lua
@@ -1,28 +1,27 @@
 local M = {}
 
 local auto = false
+local pre_yank_pos = {}
+local my_register = ""
+
+-- Function to save the cursor position before yanking
+function M.pre_yank_motion()
+	pre_yank_pos = vim.api.nvim_win_get_cursor(0)
+	my_register = vim.v.register == "" and '"' or vim.v.register
+end
+
+-- Function to restore the cursor position after yanking
+local function post_yank_motion()
+	local operators = { "y" } -- Add more operators here if needed
+	if vim.tbl_contains(operators, vim.v.event.operator) then
+		vim.api.nvim_win_set_cursor(0, pre_yank_pos)
+	end
+end
 
 local function setup_autocmds()
-	local pre_yank_pos = {}
-
-	-- Function to save the cursor position before yanking
-	local function pre_yank_motion()
-		pre_yank_pos = vim.api.nvim_win_get_cursor(0)
-	end
-
-	-- Function to restore the cursor position after yanking
-	local function post_yank_motion()
-		local operators = { "y" } -- Add more operators here if needed
-		if vim.tbl_contains(operators, vim.v.event.operator) then
-			vim.api.nvim_win_set_cursor(0, pre_yank_pos)
-		end
-	end
-
 	vim.api.nvim_create_autocmd({ "VimEnter", "CursorMoved" }, {
 		callback = function()
-			-- if auto then -- first time using <yamove>  gies error as preyankmotion is not called when auto==false -- fixed it
-			pre_yank_motion()
-			-- end
+			M.pre_yank_motion()
 		end,
 	})
 	vim.api.nvim_create_autocmd("TextYankPost", {
@@ -34,6 +33,27 @@ local function setup_autocmds()
 	})
 end
 
+local function yank_operator(type)
+	-- Determine the yank command based on the type
+	local register = my_register == "" and '"' or my_register
+	local yank_cmd = '"' .. register .. "y"
+	local expr
+
+	if type == "char" then
+		expr = "`[v`]"
+	elseif type == "line" then
+		expr = "'[V']"
+	elseif type == "block" then
+		expr = "`[<C-v>`]"
+	end
+
+	vim.cmd("normal! " .. expr .. yank_cmd)
+	-- Move cursor to the beginning of yanked text if auto is false
+	if not auto then
+		vim.cmd("normal! `[")
+	end
+end
+
 -- Move the cursor to the start - default behavior
 function M.default_yank_operator(type)
 	-- Save current auto state
@@ -41,15 +61,7 @@ function M.default_yank_operator(type)
 	-- Disable auto behavior
 	auto = false
 	-- Yank based on the current mode and count
-	if type == "char" then
-		vim.cmd('normal! `[v`]"' .. vim.v.register .. "y")
-	elseif type == "line" then
-		vim.cmd('normal! `[V`]"' .. vim.v.register .. "y")
-	elseif type == "block" then
-		vim.cmd('normal! `[<C-V>`]"' .. vim.v.register .. "y")
-	end
-	-- Move cursor to the beginning of yanked text
-	vim.cmd("normal! `[")
+	yank_operator(type)
 	-- Restore auto state
 	auto = prev_auto
 end
@@ -58,16 +70,10 @@ end
 function M.special_yank_operator(type)
 	-- Save current auto state
 	local prev_auto = auto
-	-- Disable auto behavior
+	-- Enable auto behavior
 	auto = true
 	-- Yank based on the current mode and count
-	if type == "char" then
-		vim.cmd('normal! `[v`]"' .. vim.v.register .. "y")
-	elseif type == "line" then
-		vim.cmd('normal! `[V`]"' .. vim.v.register .. "y")
-	elseif type == "block" then
-		vim.cmd('normal! `[<C-V>`]"' .. vim.v.register .. "y")
-	end
+	yank_operator(type)
 	-- Restore auto state
 	auto = prev_auto
 end
@@ -78,32 +84,32 @@ function M.setup(opts)
 
 	setup_autocmds()
 
-	vim.keymap.set(
-		"n",
-		"<Plug>(YADefault)",
-		":set operatorfunc=v:lua.require'assassin'.default_yank_operator<CR>g@",
-		{ noremap = true, silent = true }
-	)
+	vim.keymap.set("n", "<Plug>(YADefault)", function()
+		M.pre_yank_motion()
+		vim.cmd(":set operatorfunc=v:lua.require'assassin'.default_yank_operator")
+		return "g@"
+	end, { expr = true, noremap = true, silent = true })
 
-	vim.keymap.set(
-		{ "x", "v" },
-		"<Plug>(YADefault)",
-		":<C-U>call v:lua.require'assassin'.default_yank_operator()<CR>",
-		{ noremap = true, silent = true }
-	)
+	vim.keymap.set("x", "<Plug>(YADefault)", function()
+		local prev_auto = auto
+		auto = false
+		vim.cmd("normal! " .. '"' .. vim.v.register .. "y")
+		vim.cmd("normal! `[")
+		auto = prev_auto
+	end, { noremap = true, silent = true })
 
-	vim.keymap.set(
-		"n",
-		"<Plug>(YAMove)",
-		":set operatorfunc=v:lua.require'assassin'.special_yank_operator<CR>g@",
-		{ noremap = true, silent = true }
-	)
-	vim.keymap.set(
-		{ "x", "v" },
-		"<Plug>(YAMove)",
-		":<C-U>call v:lua.require'assassin'.special_yank_operator()<CR>",
-		{ noremap = true, silent = true }
-	)
+	vim.keymap.set("n", "<Plug>(YANoMove)", function()
+		M.pre_yank_motion()
+		vim.cmd(":set operatorfunc=v:lua.require'assassin'.special_yank_operator")
+		return "g@"
+	end, { expr = true, noremap = true, silent = true })
+
+	vim.keymap.set("x", "<Plug>(YANoMove)", function()
+		local prev_auto = auto
+		auto = true
+		vim.cmd("normal! " .. '"' .. vim.v.register .. "y")
+		auto = prev_auto
+	end, { noremap = true, silent = true })
 end
 
 return M

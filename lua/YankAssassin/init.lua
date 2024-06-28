c08ad31c68a9b3750f2f0d3ef2836801fa5f6cc2
svban <shb1096@gmail.com>
2024-06-28 10:04:06 +0530

    removed register saving from pre_yank_motion()

--- lua/YankAssassin/init.lua
+++ lua/YankAssassin/init.lua
@@ -3,12 +3,16 @@ local M = {}
 local auto_normal = false
 local auto_visual = false
 local pre_yank_pos = {}
+
+-- To save register for mappings
 local my_register = ""
+local function save_register()
+	my_register = vim.v.register == "" and '"' or vim.v.register
+end
 
 -- Function to save the cursor position before yanking
 local function pre_yank_motion()
 	pre_yank_pos = vim.api.nvim_win_get_cursor(0)
-	my_register = vim.v.register == "" and '"' or vim.v.register
 end
 
 -- Function to restore the cursor position after yanking
@@ -39,9 +43,7 @@ end
 
 -- Copy of vim's default yank operator
 local function yank_operator(type)
-	local register = my_register == "" and '"' or my_register
 	local expr
-
 	-- Determine the yank command based on the type
 	if type == "char" then
 		expr = "`[v`]"
@@ -51,7 +53,7 @@ local function yank_operator(type)
 		expr = "`[<C-v>`]"
 	end
 
-	vim.cmd("normal! " .. expr .. '"' .. register .. "y")
+	vim.cmd("keepjumps normal! " .. expr .. '"' .. my_register .. "y")
 end
 
 -- Move the cursor to the start - default behavior
@@ -76,25 +78,28 @@ function M.setup(opts)
 	end
 
 	vim.keymap.set("n", "<Plug>(YADefault)", function()
-		pre_yank_motion()
+		save_register()
 		vim.go.operatorfunc = "v:lua.require'YankAssassin'.default_yank_operator"
 		return "g@"
 	end, { expr = true, noremap = true, silent = true })
 
 	vim.keymap.set({ "x", "v" }, "<Plug>(YADefault)", function()
-		vim.cmd("normal! " .. '"' .. vim.v.register .. "y")
+		save_register()
+		vim.cmd("normal! " .. '"' .. my_register .. "y")
 		vim.cmd("normal! `[")
 	end, { noremap = true, silent = true })
 
 	vim.keymap.set("n", "<Plug>(YANoMove)", function()
+		save_register()
 		pre_yank_motion()
 		vim.go.operatorfunc = "v:lua.require'YankAssassin'.special_yank_operator"
 		return "g@"
 	end, { expr = true, noremap = true, silent = true })
 
 	vim.keymap.set({ "x", "v" }, "<Plug>(YANoMove)", function()
+		save_register()
 		pre_yank_motion()
-		vim.cmd("normal! " .. '"' .. vim.v.register .. "y")
+		vim.cmd("normal! " .. '"' .. my_register .. "y")
 		post_yank_motion()
 	end, { noremap = true, silent = true })
 end
